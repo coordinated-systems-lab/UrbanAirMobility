1ffb189011ca03a2ea6c78b260312dfd5123595e
aadit <farhanaadit@gmail.com>
2023-05-25 18:30:55 -0500

    completed circle.py except the plotting function.

--- src/circle.py
+++ src/circle.py
@@ -1,9 +1,12 @@
 import abstractshape as abs_shp
 import cartesian2 as c2
-import numpy as np
+import random 
+import math 
+import numpy as np 
 
 class Circle(abs_shp.AbstractShape):
     def __init__(self, center_ponint:c2.Cartesian2, radius:float, r_distribution, angle_distribution, clip:bool):
+        # r_distribution/angle_distribution = random.uniform(a,b) 
         # r_distribution/angle_distribution = np.random.random_sample() or np.random.binomial() or ...... choose any from numpy 
         self.center_point = center_ponint
         self.radius = radius
@@ -15,7 +18,8 @@ class Circle(abs_shp.AbstractShape):
     #factory function for standard circle 
     def circle(cls,center_point:c2.Cartesian2, radius:float):
         assert radius >= 0 
-        return cls(center_point, radius, radius * np.random.random_sample(), 2*np.pi * np.random.random_sample(), False)
+        #             using uniform dist choose a val between 0 and radius,  
+        return cls(center_point, radius, radius * random.uniform(0,radius), random.uniform(0, 2*math.pi), False)
     
     def getNearestPointOnEdge(self, point:c2.Cartesian2):
         vector = self.center_point - point
@@ -28,10 +32,22 @@ class Circle(abs_shp.AbstractShape):
 
         return edge_point, distance_to_edge
     
-    def samplePoint(self, rng):
-        r = self.r_distribution
-        angle = self.angle_distribution
+    def samplePoint(self, rng = random.Random(123)): #using a defaut seed of 123, should this be a variable in future while we work with testing the simulator
+        r = self.r_distribution # := random.uniform(a,b) choose a random value for radius,r using uniform dist 
+        angle = self.angle_distribution #:= random.uniform(a,b) choose a random value for angle using uniform dist
 
         if self.clip :
-            
+            r = np.clip(r, -1 * self.radius, self.radius)
+        
+        x = r * math.cos(angle)
+        y = r * math.sin(angle) 
+
+        return c2.Cartesian2(x+self.center_point.x, y+self.center_point.y) 
+    
+    #todo need to work with matplotlib, alternative - look for other plotting tools  
+    def plotShape(self, ax, color, ls):
+        pass
+
+    def getArea (self):
+        return math.pi * self.radius**2
     
