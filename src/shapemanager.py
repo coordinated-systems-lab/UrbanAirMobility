2b7fad10dd31ddb45076f87dab5878e0fa7264c3
aadit <farhanaadit@gmail.com>
2023-05-30 13:03:02 -0500

    completed shapemanager.py, check the todo and ? during unit tests

--- src/shapemanager.py
+++ src/shapemanager.py
@@ -5,11 +5,14 @@ from rectangle import Rectangle as Rect
 from typing import List, Generator
 import math
 import random
+import numpy as np
 class ShapeManger:
     def __init__(self, shapes:List[*AbsShp], weights:List[float], total_area:float):
         self.shapes = shapes
         self.weights = weights 
         self.total_area = total_area
+
+
     
     @classmethod
     def shapemanager(cls):
@@ -36,8 +39,42 @@ class ShapeManger:
     def samplePoint(self, rng:Generator):
         if len(self.shapes) == 0:
             raise RuntimeError ('NO shapes to sample from')
+                    
+        some_shape = random.choices(self.shapes, self.weights)
+
+        # ? should we provide a default seed value to reduce variablity in the code during code tests/ unit test
+        return some_shape[0].samplePoint(rng=np.random.default_rng()) 
+    
+    def sampleShapeIndex(self, rng = np.random.default_rng()):
+        if len(self.shapes) == 0:
+            raise RuntimeError ('NO shapes to sample from')
         
-        some_shape = random.choice(self.shapes)
+        # ? if self.weights are floating point numbers then index will be a float, this will raise error down the line. But if weights are ints then it won't 
+        # TODO might need to convert the index to a integer, 
+        index = random.choices(self.weights, weights = self.weights)
+        return index[0]
+    
+    def isEmpty(self,):
+        return len(self.shapes) == 0
+    
+    def getArea(self,):
+        return self.total_area
+    
+    # this function should use pyplot to plot the shape
+# ax = the graph, look at other examples its complicated
+# color = string such as "r", "g", see pyplot examples
+# ls = line shape, such as "-", "--", ":", etc. see pyplot
+
+    def render(self, ax, color,ls):
+        for shape in self.shapes:
+            shape.plotShape(ax,color,ls)
+
+    
+
+
+    
+
+
 
 
 
