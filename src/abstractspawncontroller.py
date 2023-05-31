af86b2405bc859210f6851e23b00d7ba6e4b459d
aadit <farhanaadit@gmail.com>
2023-05-31 15:30:26 -0500

    added return type to method:getSourceandDestinations

--- src/abstractspawncontroller.py
+++ src/abstractspawncontroller.py
@@ -1,13 +1,14 @@
 # Dependencies: Cartesian2, Polar2, ConvertCoordinateSystem, Dynamics, Aircraft
 from abc import abstractmethod, ABC
-from typing import List
+from typing import List, Tuple
+import cartesian2 as c2
 
 class AbstractSpawnController(ABC):
     
     
     # must return a vector of Tuple of cartesian 2 points
     @abstractmethod
-    def getSourceAndDestinations(self, timestep, current_time, aircraft, ego_position, rng,) -> List[object]:
+    def getSourceAndDestinations(self, timestep, current_time, aircraft, ego_position, rng,) -> List[Tuple[c2.Cartesian2, c2.Cartesian2]]:
         pass 
 
     @abstractmethod
