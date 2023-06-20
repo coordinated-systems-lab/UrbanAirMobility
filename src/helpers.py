0d9892fbfc04f695da992079109e2e9166e3308f
aadit <farhanaadit@gmail.com>
2023-06-20 00:46:06 -0500

    added maktInt method

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,17 @@
 # dependencies: none
-
+import random
 # converts a spawnrate measured in flights per km^2 hours to flights per m^2 seconds
+rng = random.random()
+
 def km_sq_hrs_to_m_sq_secs(spawnrate):
     return spawnrate * (1/1000)**2 * (1/3600)
 
+def makeInt(x, rng = rng):
+    ret = int(x)
+
+    decimal = x - ret
+
+    if rng < decimal:
+        ret += 1
+    
+    return ret
