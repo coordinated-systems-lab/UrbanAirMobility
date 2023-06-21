afaa4706b002f859662b07ddb48cb82c82a25fb6
aadit <farhanaadit@gmail.com>
2023-06-21 16:55:40 -0500

    modules with updated format

--- src/queuedandtimedspawncontroller.py
+++ src/queuedandtimedspawncontroller.py
@@ -1,3 +1,105 @@
+from typing import List, Tuple
+import numpy as np 
+
 import abstractspawncontroller as absspwnctrl
+import aircraft as ac
 import shapemanager as shpmng
 import cartesian2 as c2
+import circle, rectangle
+import helpers 
+
+class QueuedAndTimedSpawnController(absspwnctrl.AbstractSpawnController):
+
+    def __init__(self,
+                 sources:shpmng.ShapeManger,
+                 source_priority_functions:List,
+                 destinations:shpmng.ShapeManger,
+                 destination_priority_functions:List,
+                 queues:np.ndarray,
+                 spawnrate_per_second:float,
+                 relative_destination:bool):
+        
+        self.sources = sources
+        self.source_priority_functions = source_priority_functions
+        self.destinations = destinations
+        self.destination_priority_functions = destination_priority_functions
+        self.queues = queues
+        self.spawnrate_per_second = spawnrate_per_second
+        self.relative_destination = relative_destination
+    
+    @classmethod
+    def queuedandtimedspawncontroller(cls,
+                                      sources:shpmng.ShapeManger,
+                                      source_priority_functions:List,
+                                      destinations:shpmng.ShapeManger,
+                                      destination_priority_functions:List,
+                                      spawns_per_km_squared_hours:float,
+                                      relative_destination:bool):
+        
+        spawns_per_meter_squared_seconds = helpers.km_sq_hrs_to_m_sq_secs(spawns_per_km_squared_hours)
+        spawns_per_seconds = spawns_per_meter_squared_seconds * sources.getArea() # TODO rename to spawns_per_second
+        queues = np.zeros(shape=len(sources.shapes))
+
+        return cls(sources, source_priority_functions, destinations, destination_priority_functions, queues, spawns_per_seconds,relative_destination)
+    
+    # must return a vector of Tuple of cartesian 2 points
+    def getSourceAndDestinations(self, timestep, current_time, aircraft, ego_position, rng) -> List[Tuple[c2.Cartesian2, c2.Cartesian2]]:
+        # ac_to_spawn = helpers.makeInt(self.spawnrate_per_second * timestep)
+        # ret = []
+
+        # src_weights = []
+        # dest_weights = []
+
+        # for f in self.source_priority_functions:
+        #     src_weights.append(f(current_time))
+        
+        # for f in self.destination_priority_functions:
+        #     dest_weights.append(f(current_time))
+        
+        #  # randomly add to queues based on priorities given in shape manager
+        #  for i in range(ac_to_spawn):
+        #     index = 
+        pass
+    
+
+    def setSpawnRate(self, spawns_per_km_squared_hours):
+        spawns_per_meter_squared_seconds = helpers.km_sq_hrs_to_m_sq_secs(spawns_per_km_squared_hours)
+        spawns_per_seconds = spawns_per_meter_squared_seconds * self.sources.getArea()
+        self.spawnrate_per_second = spawns_per_seconds
+
+    def isAirspaceClear(self, point:c2.Cartesian2, acft:List[ac.Aircraft]):
+        for plane in acft:
+            if c2.Cartesian2.__abs__(plane.dynamic.position - point) < 150:
+                return False
+        return True
+    
+    def render(self, ax):
+        render(self.sources, ax, "g", ":") #TODO need to find where this render function comes from 
+        render(self.destinations, ax, "b",":")
+
+        for i in range(len(self.sources.shapes)):
+            if type(self.sources.shapes[i]) == circle.Circle:
+                x = self.sources.shapes[i].center_point.x #type: ignore
+                y = self.sources.shapes[i].center_point.y + self.sources.shapes[i].radius #type:ignore
+                s = str(self.queues[i])
+                plt.text(x,y,s) #type: ignore
+            elif type(self.sources.shapes[i]) == rectangle.Rectangle:
+                x = self.sources.shapes[i].top_left.x - 10 #type: ignore
+                y = self.sources.shapes[i].bottom_right.y + 10 #type:ignore
+                s = str(self.queues[i])
+                plt.text(x,y,s) #type: ignore
+            else:
+                print('Shapes not supported')
+
+
+    def getSinusoidal(self, length_of_cycle, max_value, min_value, peak_time):
+        assert length_of_cycle > 0
+        assert max_value >= min_value
+        assert 0<= peak_time <= length_of_cycle
+
+        magnitude = (max_value - min_value) / 2
+        adjustment = min_value + magnitude 
+        frequency = (2 * np.pi) / length_of_cycle
+
+        return lambda x: magnitude * np.cos(frequency * (x - peak_time) + adjustment) 
+    
--- src/queuedspawncontroller.py
+++ src/queuedspawncontroller.py
@@ -1,109 +1,42 @@
-# Dependencies: Cartesian2, Polar2, ConvertCoordinateSystem, Dynamics, Aircraft
+import numpy as np
+from typing import List, Tuple
+
+import abstractspawncontroller as abscntl
+import shapemanager as shpmng
+import cartesian2 as c2
+import helpers
+import aircraft as acft
 
 
-mutable struct QueuedSpawnController <: AbstractSpawnController
-    sources::ShapeManager
-    destinations::ShapeManager
-    queues::Vector{Int}
-    spawnrate_per_second::Number
-    relative_destination::Bool
-end
+class QueuedSpawnController(abscntl.AbstractSpawnController):
+    def __init__(
+        self,
+        sources: shpmng.ShapeManger,
+        destinations: shpmng.ShapeManger,
+        queues: List[int] | np.ndarray,
+        spawnrate_per_second,
+        relative_destination: bool,
+    ):
+        self.sources = sources
+        self.destinations = destinations
+        self.queues = queues
+        self.spawnrate_per_second = spawnrate_per_second
+        self.relative_destination = relative_destination
 
+    @classmethod
+    def queuedspawncontroller(
+        cls,
+        sources: shpmng.ShapeManger,
+        destinations: shpmng.ShapeManger,
+        spawns_per_km_squared_hours,
+        relativea_destination: bool,
+    ):
+        spawns_per_seconds = (
+            helpers.km_sq_hrs_to_m_sq_secs(spawns_per_km_squared_hours)
+            * sources.getArea()
+        )
+        queues = np.zeros(len(sources.shapes))
 
-function QueuedSpawnController(;        sources::ShapeManager,
-                                        destinations::ShapeManager,
-                                        spawns_per_km_squared_hours::Number,
-                                        relative_destination::Bool)
-
-    spawns_per_meter_squared_seconds = KMSquaredHoursstoMSquaredSeconds(spawns_per_km_squared_hours)
-    spawns_per_seconds = spawns_per_meter_squared_seconds * getArea(sources)
-    queues = zeros(length(sources.shapes))
-    return QueuedSpawnController(sources, destinations, queues, spawns_per_seconds, relative_destination)
-end
-
-
-# must return a vector of Tuple of cartesian 2 points
-function getSourceAndDestinations(sc::QueuedSpawnController, timestep::Number, current_time::Number, aircraft::Vector{Aircraft}, ego_position::Cartesian2, rng::AbstractRNG)
-    # foind how many to spawn this timestep
-    ac_to_spawn = makeInt(sc.spawnrate_per_second * timestep, rng)
-    ret = Vector{Tuple{Cartesian2, Cartesian2}}(undef, 0)
-
-    
-    # randomly add to queues based on priorities given in shape manager
-    for i in 1:ac_to_spawn
-        index = sampleShapeIndex(sc.sources, rng)
-        sc.queues[index] += 1
-    end
-    
-    # try to pull from queues based on if there is anyone around
-    for i in 1:length(sc.queues)
-        # if no one in queue, skip
-        if sc.queues[i] == 0 
-            continue
-        end
-
-        # sample starting point from the shape at this index
-        # this will be where the person in queue spawns
-        start = samplePoint(sc.sources.shapes[i], rng) + ego_position
-        
-        # verify there is no immediate NMAC
-        if !(isAirspaceClear(start, aircraft))
-            # println("Failed to spawn ", start, " becuase there is an ac")
-            continue
-        end
-        
-        # if not, then find their destination
-        destination = samplePoint(sc.destinations, rng)
-        if sc.relative_destination
-            destination += start
-        end
-
-        # add to list, decrement queue
-        push!(ret, (start, destination))
-        sc.queues[i] -= 1
-    end
-
-    # return final list
-    return ret
-end
-
-function setSpawnRate(sc::QueuedSpawnController, spawns_per_km_squared_hours::Number)
-    spawns_per_meter_squared_seconds = KMSquaredHoursstoMSquaredSeconds(spawns_per_km_squared_hours)
-    spawns_per_seconds = spawns_per_meter_squared_seconds * getArea(sc.sources)
-    sc.spawnrate_per_second = spawns_per_seconds
-end
-
-function isAirspaceClear(point::Cartesian2, ac::Vector{Aircraft})
-    for plane in ac
-        if Magnitude(plane.dynamic.position - point) < 150.0 # lets assume 150 meters is too close for a ac taking off!
-            return false
-        end
-    end
-
-    return true
-end
-
-
-function render(sc::QueuedSpawnController, ax)
-	# render the src and destination
-    render(sc.sources, ax, "g", ":") # dotted line
-	render(sc.destinations, ax, "b", ":") # dotted line
-
-    # render queue counts
-    for i in 1:length(sc.sources.shapes)
-        if typeof(sc.sources.shapes[i]) == Circle
-            x = sc.sources.shapes[i].center_point.x 
-            y = sc.sources.shapes[i].center_point.y + sc.sources.shapes[i].radius
-            s = string(sc.queues[i])
-            plt.text(x, y, s)
-        elseif typeof(sc.sources.shapes[i]) == Rectangle
-            x = sc.sources.shapes[i].top_left.x - 10
-            y = sc.sources.shapes[i].top_left.y + 10
-            s = string(sc.queues[i])
-            plt.text(x, y, s)
-        else
-            # TODO any other shapes you create must go here
-            println("Shape not supported for queue rendering")
-        end
-    end
-end
+        return cls(
+            sources, destinations, queues, spawns_per_seconds, relativea_destination
+        )
