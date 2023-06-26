7d2bcec7ffda0f815215f6e3f4accb3cf701e323
aadit <farhanaadit@gmail.com>
2023-06-26 14:28:38 -0500

    redefined function wgo_spanw_funciton, now rng has default value in argument.

--- src/spawnfunction.py
+++ src/spawnfunction.py
@@ -11,9 +11,9 @@ from cartesian2 import Cartesian2 as c2
 from polar2 import Polar2 as p2
 import convertcoordinatesystem as ccs
 
-def ego_spawn_function(boundary:c2, ego_location:c2, detection_radius, arrival_radius, rng): #why is this function using so many inputs when only one is being used
-    init_x = np.random.uniform(0,boundary.x) # need to check the data type, here init_x is float, julia outputs it to type rng
-    init_y = np.random.uniform(0,boundary.y) # same as before, chk data type of 'rng'
+def ego_spawn_function(boundary:c2, ego_location:c2, detection_radius, arrival_radius, rng = np.random.default_rng(123)): #why is this function using so many inputs when only one is being used
+    init_x = rng.uniform(0,boundary.x) # need to check the data type, here init_x is float, julia outputs it to type rng
+    init_y = rng.uniform(0,boundary.y) # same as before, chk data type of 'rng'
     
     dest_theta = (2*np.pi) * np.random.random_sample()
     dest_offset = ccs.toCartesian(p2(5000,dest_theta))
