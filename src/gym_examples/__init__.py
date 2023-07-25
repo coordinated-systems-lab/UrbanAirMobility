8785862f2c0665e2a2d008b100c77f8f2f625235
chief-khanman <farhanaadit@gmail.com>
2023-07-25 12:41:38 -0500

    changed the directory level of gym_examples

---
+++ src/gym_examples/__init__.py
@@ -0,0 +1,9 @@
+#__init__.py
+
+from gymnasium.envs.registration import register
+
+register(
+     id="gym_examples/UAM-v0",
+     entry_point="gym_examples.envs:UAM_Env",
+     max_episode_steps=300,
+)
---
+++ src/gym_examples/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_examples.envs.uam_env import UAM_Env
