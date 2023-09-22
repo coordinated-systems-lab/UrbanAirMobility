33ac1e0b4d6f347f74e4973229514e2126909b75
Aadit Farhan Khan <farhanaadit@gmail.com>
2023-09-22 01:55:57 -0500

    updated experiment to work with 2 agents. Unusual behaviour of script, runs experiment twice.

--- src/experiment/random_test.py
+++ src/experiment/random_test.py
@@ -3,13 +3,13 @@ from gym_examples.envs.uam_env import UAM_Env
 import gymnasium
 
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3 import A2C
+from stable_baselines3 import A2C, PPO
 #the following import is for making vectorized envs and testing trained agent in multiple vectorized env 
 from stable_baselines3.common.env_util import make_vec_env
 
 #!
 #* INSTANTIATE THE ENV 
-env = gymnasium.make('gym_examples/UAM-v0')
+env = UAM_Env()
 #* INSTANTIATE THE VECTORIZED ENV 
 vec_env = make_vec_env(UAM_Env, n_envs=1)
 # check_env(env)
@@ -29,21 +29,44 @@ vec_env = make_vec_env(UAM_Env, n_envs=1)
 #!
 
 # TRAINING AGENT 
-model = A2C("MlpPolicy", env, verbose=1).learn(100)
+model_a2c = A2C("MlpPolicy", env, verbose=1).learn(5)
+print("A2C training complete")
+model_ppo = PPO("MlpPolicy", env, verbose=1, n_steps=32, n_epochs=2).learn(5)
+print("PPO training complete")
 
-# TESTING THE TRAINED AGENT 
 obs = vec_env.reset()
-n_step = 20
+
+
+n_step = 5
+
 
 for step in range(n_step):
-    action, _ = model.predict(obs, deterministic=True)
+    action_1, _ = model_a2c.predict(obs, deterministic=True)
+    action_2, _ = model_ppo.predict(obs,deterministic=True)
     print(f"Step {step +1}")
-    print("Action: ", action)
-    obs, reward, done, info = vec_env.step(action)
-    print("obs= ", obs, "reward= ", reward, "done= ", done)
+    print("Action ego: ", action_1)
+    print("Action next to ego: ", action_2)
     
-    if done:
+    if action_1.shape == (1,2):
+        action_1 = action_1[0]
+    else:
+        action_1 = action_1
+    print(action_1.shape)
+    
+    if action_2.shape == (1,2):
+        action_2 = action_2[0]
+    else:
+        action_2 = action_2
+    print(action_2.shape)
+    obs, reward, terminated, truncated, info = env.step(action_1, action_2)
+    # vec_env is UAM_Env obj          step is from UAM_Env class 
+    
+    print("obs= ", obs, "reward= ", reward, "terminated= ", terminated, "truncated= ", truncated)
+    
+    if terminated:
         print("Goal reached!", "reward= ", reward)
         break
-
+    elif truncated:
+        print("Truncated for test step completion")
+        break
 
